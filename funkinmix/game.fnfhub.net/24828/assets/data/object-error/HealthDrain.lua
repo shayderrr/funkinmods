fd9b80714fcd1c99717de41173d6a9031fceee6d
shayderrr <pranavbajjuri@gmail.com>
2026-02-06 17:37:45 -0600

    a

--- funkinmix/game.fnfhub.net/24828/assets/data/object-error/HealthDrain.lua
+++
@@ -1,7 +0,0 @@
-function opponentNoteHit()
-    health = getProperty('health')
-    if getProperty('health') > 0.022 then
-        setProperty('health', health- 0.02);
-    end
-end
-
--- funkinmix/game.fnfhub.net/24828/assets/data/s.o.s/HealthDrain.lua
+++
@@ -1,9 +0,0 @@
-function opponentNoteHit()
-    health = getProperty('health')
-    if difficulty == 2 then
-        if getProperty('health') > 0.022 then
-            setProperty('health', health - 0.02);
-        end
-    end
-end
-
